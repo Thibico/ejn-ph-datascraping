ecf9f274980fa5aefc91fc60cbb70210858676ea
gmgniap <thetpaing@utycc.edu.mm>
2023-10-02 15:04:56 +0630

    PowerBI dashboard scraping finished.

---
+++ powerbi_scraping/parse_result_json.py
@@ -0,0 +1,168 @@
+import json
+import os
+import pandas as pd
+
+ROW_DATA = "C"
+UNCHANGED_DATA_BITMASK = "R"
+NULL_DATA_BITMASK = "Ø"
+
+METADATA_KEY_NAME = "S"
+LOOKUP_TABLE_ID = "DN"
+
+"""
+Based on some random googling, this appears to be the 
+possible data types.
+
+    enum PrimitiveType {
+        Null = 0,
+        Text = 1,
+        Decimal = 2,
+        Double = 3,
+        Integer = 4,
+        Boolean = 5,
+        Date = 6,
+        DateTime = 7,
+        DateTimeZone = 8,
+        Time = 9,
+        Duration = 10,
+        Binary = 11,
+        None = 12,
+    }
+
+"""
+COL_DATA_TYPE = "T"
+TYPE_TEXT = 1
+TYPE_DATETIME = 7
+
+def extract_raw_data(j):
+    return j['results'][0]['result']['data']['dsr']['DS'][0]['PH'][0]["DM0"]
+
+def extract_data_compression_table(j):
+    return j['results'][0]['result']['data']['dsr']['DS'][0]["ValueDicts"]
+
+def extract_query_metadata(j):
+    return j['results'][0]['result']['data']['descriptor']['Select']
+
+def extract_column_count(j):
+    m = extract_query_metadata(j)
+    return len(m)
+
+def extract_column_metadata(j):
+    # why they store this in the first result i do not know
+    # this contains (1) what datatype the column is and 
+    # (2) what lookup table key should we use to lookup data
+    r = extract_raw_data(j)
+    return r[0][METADATA_KEY_NAME]
+
+def extract(n):
+    # the end result array we'll be returning
+    result = []
+
+    query_results = extract_raw_data(n) 
+    lookup = extract_data_compression_table(n)
+
+    # We'll need this for loops and calculations.
+    num_cols = extract_column_count(n)
+
+    # This presizes an array the same size as our data
+    # Part of the data compression algo is dropping columns 
+    # if the data is the same as the previous row. So we'll need
+    # to keep track of last values at all times.
+    last_value_cache = [None] * num_cols
+
+    # Column metadata contains what datatype the column is, and
+    # which lookup key we should use to get compressed data
+    column_metadata = extract_column_metadata(n)
+
+    for entry in query_results: 
+        # this eventually will be the final uncompressed row
+        row = []
+
+        raw_data = entry[ROW_DATA]
+        
+        # we're going to be popping off values from this array
+        # in a minute. reversing the array makes this O(2n) instead of O(n^2)
+        raw_data.reverse()
+        
+        # indicates what columns are missing because the data is the same as last row
+        # if the mask key is not present, that means no columns removed
+        unchanged_mask = entry.get(UNCHANGED_DATA_BITMASK, 0)
+
+        # indicates what columns are missing because they're null
+        # if the mask key is not present, that means no columns removed
+        null_mask = entry.get(NULL_DATA_BITMASK, 0)
+
+        # for each column in this particular row....
+        for n in range(0, num_cols):
+            # this will hold the final value of the current cell after we
+            # untangle everything
+            current_cell = None
+
+            # If you haven't seen the bitshift operator before(<<), this 
+            # bit of arcana means that from 0 to NUM_COLS, we can generate a binary
+            # number like 000001, 000010, 000100, etc etc.
+            #
+            # This generates a bitmask for the current row. 
+            #
+            # if we bitwise AND the current row with the "is data missing" bitmasks,
+            # it will return true (non-zero) if col n is missing from our raw data
+            #
+            # For example, if the unchanged bitmask is 001010, then:
+            #
+            # col0: 0010010 & 000001 = 000000 = false
+            # col1: 0010010 & 000010 = 000010 = true 
+            # col2: 0010010 & 000100 = 000000 = false
+             
+            col_mask = 1 << n
+
+            # is the current column missing from the raw row data? 
+            if unchanged_mask & col_mask: # YES, because it's unchanged
+                current_cell = last_value_cache[n]
+            elif null_mask & col_mask: # YES, because it's null
+                current_cell = None
+            else: # NO
+                current_cell = raw_data.pop()
+                # since we have a value, store it in the last value cache
+                last_value_cache[n] = current_cell
+
+            metadata = column_metadata[n]
+            
+            # OK, now that we *have* the data, we need to figure out if we need to 
+            # format it, or if it's a reference to a lookup table.
+
+            # This current code is fairly jank, and will crash if new datatypes are
+            # added, but I don't think we'll need to worry about that for this project.
+
+            if type(current_cell) == int:
+                if metadata[COL_DATA_TYPE] == TYPE_TEXT:
+                    # this means our data is in the lookup table
+                    # and we should treat the current column as 
+                    # an array position, not a literal integer
+                    lookup_key = metadata[LOOKUP_TABLE_ID]
+                    row.append(lookup[lookup_key][current_cell])
+
+                else:
+                    # TODO throw up some kind of alerting
+                    # there's prolly other integer-like data 
+                    # types out there, including actual integers
+                    row.append(current_cell)
+            else:
+                row.append(current_cell)
+        result.append(row)
+    return(result)
+
+with open("./json_files/projected_waste_test2.json", "r") as read_file:
+    data = json.load(read_file)
+    result = extract(data)
+
+    cols = ['region', 'province', 'city_municipality', 'projected_waste', 'year']
+    
+    df = pd.DataFrame(result, columns=cols)
+    print(df.shape)
+    df.to_csv('csv_files/projected_waste.csv', encoding='utf-8')
+    print("Finish export!")
+    
+    
+    
+        
+        
--- scraping_notes.md
+++ scraping_notes.md
@@ -1,11 +1,25 @@
-# Scraping w/ PlayWright
+# PowerBI Scraping for Projected Waste Generation
+- To scrape data from PowerBI [Dashboard](https://app.powerbi.com/view?r=eyJrIjoiNjc4OTE2OTktMDdhMC00YzM1LTkwMjEtYWUxMDIyMjI0MWMwIiwidCI6ImY2ZjRhNjkyLTQzYjMtNDMzYi05MmIyLTY1YzRlNmNjZDkyMCIsImMiOjEwfQ%3D%3D&pageName=ReportSection&fbclid=IwAR264Sfm3ocnSBovLnpGgdSKXljXQeGAx9JpZIxcAS3YyV4voqVpHzPTBNw)
+- PowerBI has POST api to pull the data. So, we need to use bash script to scrape.
+- Command to run bash
+```cmd
+sh final_aste_scraping.sh
+```
+
+## Result parsing w/ Python
+- PowerBI api result is nested JSON structure and it's quite complex. We need to parse it using Python.
+
+*****
+
+# Scraping data for Philippines Port Authority website
 - Create Virtual Environment
 - Install with pip
+- Initially planned to use to scrape PPA website with playwright , but no need it anymore. Worked with requests + bs4.
 
-# Tasks to do
+## Tasks to do
 - For every web page, I need to click all to expand hidden row.
 
-# Useful Commands
+## Useful Commands
 ```
 pip3 install playwright
 ```
